f39c3c40008a78dabd703499a23c88633ff10ad1
Adrian Czuczka <adrianczuczka@gmail.com>
2017-09-25 21:38:47 +0100

    Big Changes
    Added a lot of fused location provider functionality, but some bugs, so all commented out

--- app/src/main/java/com/adrianczuczka/songle/GameUI.java
+++ app/src/main/java/com/adrianczuczka/songle/GameUI.java
@@ -1,18 +1,24 @@
 package com.adrianczuczka.songle;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,15 +26,47 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.maps.android.data.kml.KmlLayer;
-
-import java.io.InputStream;
 
 public class GameUI extends FragmentActivity implements OnMapReadyCallback {
     private FusedLocationProviderClient mFusedLocationClient;
     private GoogleMap mMap;
+    /* EXPERIMENTAL
+    private LocationRequest mLocationRequest;
+    LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+    //SettingsClient client = LocationServices.getSettingsClient(this);
+    //Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+    private boolean mRequestingLocationUpdates = true;
+    private LocationCallback mLocationCallback;
+    private LooperThread looper = new LooperThread();
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    private void startLocationUpdates() {
+        try{
+             Experimental
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                    mLocationCallback,
+                    null looper);
+
+        }
+        catch (SecurityException e){
+
+        }
+    }
+    protected void createLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +75,12 @@ public class GameUI extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        /*Experimental
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        createLocationRequest();
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+                */
     }
 
 
@@ -75,6 +119,19 @@ public class GameUI extends FragmentActivity implements OnMapReadyCallback {
             catch (SecurityException e){
                 //warning
             }
+            /* Experimental
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                // Logic to handle location object
+                                startLocationUpdates();
+                            }
+                        }
+                    });
+                   */
         }
         LatLng northWestLatLng = new LatLng(55.946233, -3.192473);
         LatLng northEastLatLng = new LatLng(55.946233, -3.184319);
@@ -97,15 +154,28 @@ public class GameUI extends FragmentActivity implements OnMapReadyCallback {
                 if (permissions.length == 1 &&
                         permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) &&
                         grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.e("GameUI","hello3");
+                    Log.e("GameUI", "hello3");
                     try {
-                        Log.e("GameUI","hello4");
+                        Log.e("GameUI", "hello4");
                         mMap.setMyLocationEnabled(true);
                         Log.e("GameUI", String.valueOf(mMap.isMyLocationEnabled()));
-                    } catch (SecurityException s) {
-                        Log.e("GameUI","error");
+                        /* Experimental
+                        mFusedLocationClient.getLastLocation()
+                                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                                    @Override
+                                    public void onSuccess(Location location) {
+                                        // Got last known location. In some rare situations this can be null.
+                                        if (location != null) {
+                                            // Logic to handle location object
+                                            startLocationUpdates();
+                                        }
+                                    }
+                                });*/
+                    } catch (SecurityException e) {
+                        Log.e("GameUI", "error");
                         //warning
                     }
+
                 } else {
                     //permission denied
                 }
@@ -116,3 +186,20 @@ public class GameUI extends FragmentActivity implements OnMapReadyCallback {
         }
     }
 }
+/* Experimental
+class LooperThread extends Thread {
+    private Handler mHandler;
+
+    public void run() {
+        Looper.prepare();
+
+        mHandler = new Handler() {
+            public void handleMessage(Message msg) {
+                // process incoming messages here
+            }
+        };
+
+        Looper.loop();
+    }
+}
+*/
