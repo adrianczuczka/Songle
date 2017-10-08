9fa4dd5b5a375b6838be307affd64da6856ab499
adrianczuczka <adrianczuczka@gmail.com>
2017-10-08 18:58:04 +0100

    Started choose song layout

--- app/src/main/java/com/adrianczuczka/songle/ChooseSong.java
+++ app/src/main/java/com/adrianczuczka/songle/ChooseSong.java
@@ -1,11 +1,21 @@
 package com.adrianczuczka.songle;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class ChooseSong extends AppCompatActivity {
 
@@ -25,5 +35,23 @@ public class ChooseSong extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    private class ParseXMLTask extends AsyncTask<String, Void, ArrayList<XMLParser.Song>>{
+        @Override
+        protected ArrayList<XMLParser.Song> doInBackground(String... strings) {
+            try {
+                InputStream stream = new ByteArrayInputStream(strings[0].getBytes(StandardCharsets.UTF_8.name()));
+                XMLParser parser = new XMLParser();
+                return parser.parse(stream);
+            }
+            catch (XmlPullParserException | IOException e) {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<XMLParser.Song> songs) {
+            Log.e("GameUI", String.valueOf(songs));
+        }
     }
 }
